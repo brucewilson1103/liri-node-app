099d2544f70124eca0db6098fff35797a4242959
da Vinci <brucewilson1103@gmail.com>
2019-01-24 13:33:54 -0500

    all searches working I just need to clean up comments and adjust teh movie search to get back specifics

--- liri.js
+++ liri.js
@@ -103,7 +103,38 @@ else if (nodeArgs[2] === "spotify-this-song") {
 
 else if (nodeArgs[2] === "movie-this") {
 
-    
+  var axios = require("axios");
+
+  // Store all of the arguments in an array
+  var nodeArgs = process.argv;
+  
+  // Create an empty variable for holding the movie name
+  var movieName = "";
+  
+  // Loop through all the words in the node argument
+  // And do a little for-loop magic to handle the inclusion of "+"s
+  for (var i = 3; i < nodeArgs.length; i++) {
+  
+    if (i > 3 && i < nodeArgs.length) {
+      movieName = movieName + "+" + nodeArgs[i];
+    }
+    else {
+      movieName += nodeArgs[i];
+  
+    }
+  }
+  
+  // Then run a request with axios to the OMDB API with the movie specified
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  
+  // // This line is just to help us debug against the actual URL.
+  // console.log(queryUrl);
+  
+  axios.get(queryUrl).then(
+    function(response) {
+      console.log("Release Year: " + response.data.Year);
+    }
+  );
   
 }
 
